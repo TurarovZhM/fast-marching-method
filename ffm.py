1e5fc75a23708f655938b79412a5a549f643aaf7
Andrey Stoyan <stoyan.yukio@gmail.com>
2019-04-21 03:35:46 +0300

    Just works

--- ffm.py
+++ ffm.py
@@ -24,81 +24,90 @@ def _neighbours(p: (int, int), mesh):
 
 
 def _val(color):
-    r, g, b, _ = color
-    return (r + g + b) / 3
+    *cs, _ = color
+    cs = list(map(lambda c: 1 - c if c < 0.5 else c, cs))
+    return sum(cs) / 3
 
 
-def _distance(p: (int, int), mesh, status):
-    def value(x: (int, int)):
-        return _val(mesh[x])
+def _f(x: (int, int), mesh):
+    return _val(mesh[x])
 
-    def choose(p1: (int, int), p2: (int, int), *_, default=0):
-        if _check(p1, mesh) and status[p1] == State.FROZEN and _check(p2, mesh) and status[p2] == State.FROZEN:
-            return min(value(p1), value(p2))
-        elif _check(p1, mesh) and status[p1] == State.FROZEN:
-            return value(p1)
-        elif _check(p2, mesh) and status[p2] == State.FROZEN:
-            return value(p2)
-        return default
+
+def _distance(p: (int, int), mesh, t, state):
+    def choose(p1: (int, int), p2: (int, int), *_, default=float('inf')):
+        if _check(p1, mesh) and state[p1] == State.FROZEN and _check(p2, mesh) and state[p2] == State.FROZEN:
+            return (t[p1], p1) if t[p1] <= t[p2] else (t[p2], p2)
+        elif _check(p1, mesh) and state[p1] == State.FROZEN:
+            return t[p1], p1
+        elif _check(p2, mesh) and state[p2] == State.FROZEN:
+            return t[p2], p2
+        return default, None
 
     up = p[0] - 1, p[1]
     down = p[0] + 1, p[1]
-    va = choose(up, down)
+    ta, pa = choose(up, down)
 
     left = p[0], p[1] - 1
     right = p[0], p[1] + 1
-    vb = choose(left, right)
+    tb, pb = choose(left, right)
 
-    assert va or vb  # TODO
+    tm, pm = (ta, pa) if ta <= tb else (tb, pb)
+    if pm is None:
+        return tm
 
-    d = (va + vb)**2 - 2 * (va**2 + vb**2 - 1)
-    if d >= 0:
-        return (va + vb + math.sqrt(d)) / 2
-    return min(va, vb) + 1  # TODO
-    # return float('inf')
+    fprev = _f(pm, mesh)
+    fcurr = _f(p, mesh)
+    dif = fcurr + 1 / (2 * abs(fcurr - fprev) + 1) - 1  # TODO
+    assert abs(dif) > 1e-5, dif
+
+    def default():
+        return min(ta, tb) + 1 / dif
+
+    if pa is None:
+        assert pb is not None
+        return default()
+    elif pb is None:
+        return default()
+
+    d = (ta + tb)**2 - 2 * (ta**2 + tb**2 - 1 / dif**2)
+    if d > 1e-10:
+        return (ta + tb + math.sqrt(d)) / 2
+    return default()
 
 
-def fmm(mesh, start, *_, fillcolor=None, field=1.0):
-    fillcolor = [0, 0, 1, 1] if fillcolor is None else fillcolor  # TODO: is needed?
+def fmm(mesh, start, *_, fillcolor=None, field=None):
+    fillcolor = [0, 1, 0, 1] if fillcolor is None else fillcolor
+    field = min(mesh.shape[:2]) / 10 if field is None else field
 
-    dist = np.array([[float('inf')] * len(mesh[0]) for _ in range(len(mesh))])
-    status = np.array([[State.FAR] * len(mesh[0]) for _ in range(len(mesh))])
-    pqueue = PQueue()
+    t = np.array([[float('inf')] * mesh.shape[1] for _ in range(mesh.shape[0])])
+    state = np.array([[State.FAR] * mesh.shape[1] for _ in range(mesh.shape[0])])
+    pq = PQueue()
 
     # init
-    dist[start] = 0
-    status[start] = State.FROZEN
-    for v in _neighbours(start, mesh):
-        dst = _distance(v, mesh, status)
-        if status[v] != State.NB:
-            status[v] = State.NB
-            dist[v] = dst
-            pqueue.push(dst, v)
-        else:
-            assert dst <= dist[v]
-            pqueue.repos(v, dst)
+    t[start] = 0
+    state[start] = State.FROZEN
+    for vn in _neighbours(start, mesh):
+        d = _distance(vn, mesh, t, state)
+        t[vn] = d
+        state[vn] = State.NB
+        pq.push(d, vn)
 
-    # loop
-    while pqueue:
-        dst, v = pqueue.pop()
-        status[v] = State.FROZEN
-        dist[v] = dst
-
+    # compute time
+    while pq:
+        vdist, v = pq.pop()
+        t[v] = vdist
+        state[v] = State.FROZEN
         for vn in _neighbours(v, mesh):
-            if status[vn] == State.FROZEN:
-                continue
-            d = _distance(vn, mesh, status)
-            if status[vn] != State.NB:
-                status[vn] = State.NB
-                pqueue.push(d, vn)
-            else:
-                # assert dist[vn] >= d  # TODO fails
-                pqueue.repos(vn, d)
+            d = _distance(vn, mesh, t, state)
+            if state[vn] == State.FAR:
+                state[vn] = State.NB
+                pq.push(d, vn)
+            elif state[vn] == State.NB:
+                pq.repos(vn, d)
 
-    # result
-    assert len(mesh) == len(dist)
-    assert len(mesh[0]) == len(dist[0])  # TODO: better way to get inner size?
-    for row in range(len(dist)):
-        for col in range(len(dist[0])):
-            if dist[row][col] <= field:
+    # results
+    for row in range(t.shape[0]):
+        for col in range(t.shape[1]):
+            if t[row][col] <= field:
+                assert state[row][col] == State.FROZEN
                 mesh[row][col] = fillcolor
--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import ffm
+import exp
 
 
 def main():
-    img = mpimg.imread('blank.png')
+    img = mpimg.imread('klaxa.png')
+    # img = mpimg.imread('brain.png')
     plt.figure(figsize=(10, 10))
     plt.imshow(img)
 
     x = plt.ginput(1)[0]
     x = (int(round(x[1])), int(round(x[0])))
-    ffm.fmm(img, x, field=0.75)
+    ffm.fmm(img, x)
     plt.imshow(img)
 
     plt.show()
